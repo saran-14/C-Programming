265c413c2984fb1f465217699b274456cf016348
saran-14 <119130387+saran-14@users.noreply.github.com>
2023-02-06 08:25:58 +0530

    Create ascending.c

---
+++ ascending.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main()
+ {
+    int n,a[100],temp;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+       scanf("%d ",&a[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                temp=a[j];
+                a[j]=a[i];
+                a[i]=temp;
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    
+}
